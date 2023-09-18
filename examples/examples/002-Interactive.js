d1c0757e31a2bf30e71b2c740082533ef207416c
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-18 21:51:53 +0700

    examples : add spring example

--- examples/examples/002-Interactive.js
+++ examples/examples/002-Interactive.js
@@ -3,9 +3,9 @@
 int.draw_function = (inp) => {
     let x = inp['x'];
 
-    let big_rect   = circle(50);
+    let big_circ   = circle(50);
     let small_rect = square(20).fill('blue').translate(V2(x,0));
-    draw(big_rect, small_rect);
+    draw(big_circ, small_rect);
 };
 
 int.slider('x', -50, 50, 0);
