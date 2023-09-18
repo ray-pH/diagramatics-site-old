a110a68ffa0ce2c4ed1d134be5e6a223bca50b30
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-18 21:58:30 +0700

    examples : cleanup

--- examples/examples/005-Sierpinski.js
+++ examples/examples/005-Sierpinski.js
@@ -1,21 +1,21 @@
 // Sierpiński triangle
 ////
 int.draw_function = (inp) => {
-  let n = inp['n'];
-  
-  let s0 = regular_polygon_side(3, 50)
-    .position().fill('lightblue').stroke('none');
-  let snext = (s) => diagram_combine(
-    s,
-    s.translate(V2(50,0)),
-    s.translate(V2(25,50*Math.sqrt(3)/2)),
-  ).scale(V2(0.5,0.5)).position().flatten();
-  let sn = (n) => {
-    if (n == 0) return s0;
-    else return snext(sn(n-1));
-  }
-  let a = sn(n);
-  draw(a);
+    let n = inp['n'];
+
+    let s0 = regular_polygon_side(3, 50)
+        .position().fill('lightblue').stroke('none');
+    let snext = (s) => diagram_combine(
+        s,
+        s.translate(V2(50,0)),
+        s.translate(V2(25,50*Math.sqrt(3)/2)),
+    ).scale(V2(0.5,0.5)).position().flatten();
+    let sn = (n) => {
+        if (n == 0) return s0;
+        else return snext(sn(n-1));
+    }
+    let a = sn(n);
+    draw(a);
 };
 
 int.slider('n', 0, 4, 1, 1);
--- examples/examples/007-Spring.js
+++ examples/examples/007-Spring.js
@@ -12,7 +12,7 @@ int.draw_function = (inp) => {
     let floor = line(V2(0,0), V2(100,0)).strokewidth(2);
     let box   = square(20).position(V2(x,10))
         .fill('lightblue').stroke('none');
-    // let spring = line(wall.get_anchor('center-right'), box.get_anchor('center-left'))
+
     let p1 = wall.get_anchor('center-right');
     let p2 = box.get_anchor('center-left');
     let spring = mechanics.spring(p1,p2, 2, 10, 1.2);
