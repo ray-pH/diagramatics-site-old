9c731bfcb9b600c052219bf22477f7d13ac70ac8
Rayhan Alghiffari Azizi <rayhan.azizi9@gmail.com>
2023-09-18 11:17:21 +0700

    examples : css styling

--- examples/index.js
+++ examples/index.js
@@ -10,8 +10,13 @@ for (let ex in examples) {
     //     <div class="example-diagram">
     //         <svg class="svg-diagram"> ... </svg>
     //     </div>
-    //     <div class="example-code"> 
-    //         ... 
+    //     <div class="example-code-container"> 
+    //         <span class="example-title"> ... </span>
+    //         <div class="example-code-bg"> 
+    //             <div class="example-code"> 
+    //                 ... 
+    //             </div>
+    //         </div>
     //     </div>
     // </div>
     let div = document.createElement('div');
@@ -23,15 +28,27 @@ for (let ex in examples) {
     svg.classList.add('svg-diagram');
     diagramdiv.appendChild(svg);
 
+    let codecontainerdiv = document.createElement('div');
+    codecontainerdiv.classList.add('example-code-container');
+
+    let title = document.createElement('span');
+    title.classList.add('example-title');
+    title.innerHTML = examples[ex].comments[0];
+
+    let codedivbg = document.createElement('div');
+    codedivbg.classList.add('example-code-bg');
     let codediv = document.createElement('div');
     codediv.classList.add('example-code');
     codediv.classList.add('hljs');
     codediv.classList.add('javascript');
 
     codediv.innerHTML = '<pre>' + hljs.highlight(examples[ex].code, { language: 'javascript' }).value + '</pre>';
+    codedivbg.appendChild(codediv);
 
+    codecontainerdiv.appendChild(title);
+    codecontainerdiv.appendChild(codedivbg);
     div.appendChild(diagramdiv);
-    div.appendChild(codediv);
+    div.appendChild(codecontainerdiv);
     containerdiv.appendChild(div);
     
     draw_code(svg, examples[ex].code);
